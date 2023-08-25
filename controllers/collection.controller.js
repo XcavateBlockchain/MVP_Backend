0a557fe233699928e97bc9cbddb1793c03fa662c
meanking <adonis.morgan.god@gmail.com>
2023-08-25 21:58:20 +0700

    - add: collection creation of loan request

--- controllers/collection.controller.js
+++ controllers/collection.controller.js
@@ -1,6 +1,7 @@
 import { StatusCodes } from 'http-status-codes'
 import { Collection } from '../models/collection.model.js'
 import { Property } from '../models/property.model.js'
+import { Loan } from '../models/loan.model.js'
 
 export const create = async (req, res) => {
   try {
@@ -12,6 +13,7 @@ export const create = async (req, res) => {
       owner,
       user: _id,
       property: propertyId,
+      type: 'property',
     })
     const doc = await collection.save()
 
@@ -63,6 +65,54 @@ export const getLastId = async (req, res) => {
         data: 0,
       })
     }
+  } catch (error) {
+    console.log('get latest collection id error :: ', error)
+    return res.status(StatusCodes.BAD_REQUEST).send({
+      error: error.toString(),
+      data: null,
+    })
+  }
+}
+
+export const createLoanCollection = async (req, res) => {
+  try {
+    const { _id } = req.user
+    const { id, owner, loanId, page } = req.body
+    
+    const collection = new Collection({
+      id,
+      owner,
+      user: _id,
+      loan: loanId,
+      type: 'loan',
+    })
+    const doc = await collection.save()
+
+    const loan = await Loan.findByIdAndUpdate(
+      loanId,
+      {
+        $set: {
+          collect: doc._id,
+          isMinted: true,
+        },
+      },
+      {
+        new: true,
+      },
+    )
+      .populate('user')
+      .populate('collect')
+
+    const loans = await Loan.find({
+      user: _id,
+    })
+      .populate('user')
+      .populate('collect')
+
+    return res.status(StatusCodes.CREATED).send({
+      error: null,
+      data: page === 'Detail' ? loan : loans,
+    })
   } catch (error) {
     console.log('get latest collection id error :: ', error)
     return res.status(StatusCodes.BAD_REQUEST).send({
